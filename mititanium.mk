4275583d8a6b55113b07c6eba69a98d8c84caaa2
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-02 10:58:06 +0000

    mititanium-common: libstdc++.vendor => libstdc++_vendor
    Signed-off-by: RenzAlt <129452476+RennAlt@users.noreply.github.com>

--- mititanium.mk
+++ mititanium.mk
@@ -132,7 +132,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.camera.device@1.0.vendor
 
 PRODUCT_PACKAGES += \
-    libstdc++.vendor
+    libstdc++_vendor
 
 # disable_configstore
 PRODUCT_PACKAGES += \
