dc82b7d82533dc1c81800a8217e5145714a850ec
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-23 14:26:14 -0400

    mititanium-common: change signed key dir
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -14,7 +14,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/userspace_reboot.mk)
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
 
 # Signed
--include vendor/extra/product.mk
+-include vendor/lineage-priv/keys/keys.mk
 
 # Bootanimation
 TARGET_BOOTANIMATION_HALF_RES := true
