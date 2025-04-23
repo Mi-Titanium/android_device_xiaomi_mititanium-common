680d1fc2ad8ac5fd58c8454d7d9879e2e61cc275
RenzAlt <129452476+RennAlt@users.noreply.github.com>
2025-04-23 16:16:15 -0400

    mititanium-common: Inherit lineage common sepolicy

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -142,6 +142,7 @@ endif
 ENABLE_VENDOR_RIL_SERVICE := true
 
 # SELinux
+include device/lineage/sepolicy/common/sepolicy.mk
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 PRODUCT_PRIVATE_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/private
