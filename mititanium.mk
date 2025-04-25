51d59178821819be4e20b27b056b273a1d98652c
Itz-Kanasama <164613153+Itz-Kanasama@users.noreply.github.com>
2025-04-25 06:17:08 +0000

    mititanium-common: Bump Keymaster to V4.0
    Signed-off-by: RennAlt <shenprjktplayground@gmail.com>

--- mititanium.mk
+++ mititanium.mk
@@ -267,7 +267,7 @@ PRODUCT_PACKAGES += \
 
 # Keymaster
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0.vendor
+    android.hardware.keymaster@4.0-service
 
 # Lights
 PRODUCT_PACKAGES += \
